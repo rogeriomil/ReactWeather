b5a6bea2cb5a5552d12b99144e4f2a5a5e3e23bb
Rogerio Nunes <rogerio.nunesp@gmail.com>
2017-05-19 18:50:21 -0300

    Updating About and Examples pages

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 
 var About = (props) => {
     return (
-        <h2>About Component</h2>
+        <div>
+            <h2>About</h2>
+            <p>Welcome to the About page.</p>
+        </div>
     )
 };
 
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
-        <h2>Examples Component</h2>
+        <div>
+        <h2>Examples</h2>
+        <p>Welcome to the Examples page</p>
+        </div>
     )
 };
 
