bd0ee314e84ee15ecf6cd98a02b1bb95c564b86c
Rogerio Nunes <rogerio.nunesp@gmail.com>
2017-05-23 13:57:33 -0300

    Grid implementation

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,23 @@
 var React = require('react');
-
+import {Link} from'react-router';
 
 var Examples = (props) => {
     return (
         <div>
-        <h2>Examples</h2>
-        <p>Welcome to the Examples page</p>
+        <h1 className="text-center">Examples</h1>
+        <p>A few examples to try:</p>
+        <ol>
+            <li>
+                <Link to='/?city=SãoPaulo'>São Paulo</Link>
+            </li>
+            <li>
+                <Link to='/?city=Campinas'>Campinas</Link>
+            </li>
+            <li>
+                <Link to='/?city=Rio de janeiro'>Rio de Janeiro</Link>
+            </li>
+        </ol>
+
         </div>
     )
 };
