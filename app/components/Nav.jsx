c89f37824f2bb6d6433d29823c56b21f699791f5
Rogerio Nunes <rogerio.nunesp@gmail.com>
2017-05-23 07:37:20 -0300

    Adding Nav style

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,15 +2,52 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 import {Link, IndexLink} from 'react-router';
 
-var Nav = (props) => {
-    return (
-            <div>
-                <h1>Nav component</h1>
-                <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
-                <Link to="/about" activeClassName="active">About</Link>
-                <Link to="/examples" activeClassName="active">Examples</Link> 
+class Nav extends React.Component {
+    constructor() {
+        super();
+        this.onSearchBar = this.onSearchBar.bind(this);
+
+    }
+    onSearchBar(evt) {
+        evt.preventDefault();        
+
+        alert('Not working yet');
+    }
+
+    render() {
+        return (
+            
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">ReactWeather</li>
+                    <li>
+                        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+                    </li>
+                    <li>
+                        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+                    </li>
+                </ul>
             </div>
+            <div className="top-bar-right">
+                <form onSubmit={(e) => this.onSearchBar(e)}>
+                    <ul className="menu">
+                        <li>
+                            <input type="search" placeholder="Search weather"/>
+                        </li>
+                        <li>
+                            <input type="submit" className="button"/>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </div>
         )
-};
+        
+    }
+}
 
 export default Nav;
